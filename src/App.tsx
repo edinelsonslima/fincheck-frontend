777f7bc6916d8998e76721751393e0702ce25de3
edinelsonslima <edinelsonlima86@gmail.com>
2023-08-29 00:14:34 -0300

    added ReactQueryDevtools configure defaultOptions QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Router } from "./router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+
+import { Router } from "./router";
 import { AuthProvider } from "./app/contexts/auth.context";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 5 * 1000,
+    },
+  },
+});
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <AuthProvider>
         <Router />
         <Toaster />
