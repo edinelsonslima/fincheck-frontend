b718541962a0c7f9f3db5c2c0a184ac8cb9fe6a5
edinelsonslima <edinelsonlima86@gmail.com>
2023-08-29 00:13:03 -0300

    created accessToken functions

--- src/app/contexts/auth.context.tsx
+++ src/app/contexts/auth.context.tsx
@@ -1,14 +1,32 @@
-import { PropsWithChildren, createContext } from "react";
+import { PropsWithChildren, createContext, useCallback } from "react";
+import { enLocalStorage } from "../../types/enums/local-storage.enum";
+import { useLocalStorage } from "../hooks/use-local-storage.hook";
 
 interface AuthContextValue {
   signedIn: boolean;
+  signout: () => void;
+  signin: (accessToken: string) => void;
 }
 
-export const AuthContext = createContext<AuthContextValue>({ signedIn: false });
+export const AuthContext = createContext<AuthContextValue>(
+  {} as AuthContextValue
+);
 
 export function AuthProvider({ children }: PropsWithChildren) {
+  const [accessToken, setAccessToken, removeAccessToken] = useLocalStorage(
+    enLocalStorage.ACCESS_TOKEN,
+    ""
+  );
+
+  const signin = useCallback(
+    (accessToken: string) => setAccessToken(accessToken),
+    [setAccessToken]
+  );
+
+  const signout = useCallback(() => removeAccessToken(), [removeAccessToken]);
+
   return (
-    <AuthContext.Provider value={{ signedIn: false }}>
+    <AuthContext.Provider value={{ signedIn: !!accessToken, signin, signout }}>
       {children}
     </AuthContext.Provider>
   );
