4b5d6db0a2549c07f1b2f3d61e05430dbaf01b43
edinelsonslima <edinelsonlima86@gmail.com>
2023-08-29 23:26:36 -0300

    added Authorization Bearer in requests

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { enLocalStorage } from "../../types/enums/local-storage.enum";
 
 class HttpClient {
   constructor(private readonly _axios: AxiosInstance) {}
@@ -33,12 +34,19 @@ class HttpClient {
     return this.request<TData, TBody>({ ...config, url, method: "DELETE" });
   }
 
-  private request<TData = unknown, TBody = unknown>(
-    config: AxiosRequestConfig<TBody>
-  ) {
-    return this._axios.request<TData, AxiosResponse<TData, TBody>, TBody>(
-      config
-    );
+  private request<TData = unknown, TBody = unknown>({
+    headers,
+    ...config
+  }: AxiosRequestConfig<TBody>) {
+    return this._axios.request<TData, AxiosResponse<TData, TBody>, TBody>({
+      ...config,
+      headers: { ...headers, Authorization: `Bearer ${this.getAccessToken()}` },
+    });
+  }
+
+  private getAccessToken() {
+    const accessToken = localStorage.getItem(enLocalStorage.ACCESS_TOKEN);
+    return JSON.parse(accessToken ?? "");
   }
 }
 
