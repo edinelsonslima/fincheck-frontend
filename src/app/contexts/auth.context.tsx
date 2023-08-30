ac64cf6776c6f1fd73335cd858e6406c22560017
edinelsonslima <edinelsonlima86@gmail.com>
2023-08-30 00:40:15 -0300

    signout when error occurs request user info data remove cache user in signout created loading screen

--- src/app/contexts/auth.context.tsx
+++ src/app/contexts/auth.context.tsx
@@ -1,6 +1,17 @@
-import { PropsWithChildren, createContext, useCallback } from "react";
-import { enLocalStorage } from "../../types/enums/local-storage.enum";
+import {
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useEffect,
+} from "react";
+import { toast } from "react-hot-toast";
+
 import { useLocalStorage } from "../hooks/use-local-storage.hook";
+import { useUserMe } from "../hooks/use-user.hook";
+
+import { LaunchScreen } from "../../view/components/page-loader.component";
+
+import { enLocalStorage } from "../../types/enums/local-storage.enum";
 
 interface AuthContextValue {
   signedIn: boolean;
@@ -18,16 +29,37 @@ export function AuthProvider({ children }: PropsWithChildren) {
     ""
   );
 
+  const { isSuccess, isFetching, isError, remove } = useUserMe({
+    enabled: !!accessToken,
+    staleTime: Infinity,
+  });
+
   const signin = useCallback(
     (accessToken: string) => setAccessToken(accessToken),
     [setAccessToken]
   );
 
-  const signout = useCallback(() => removeAccessToken(), [removeAccessToken]);
+  const signout = useCallback(() => {
+    removeAccessToken();
+    remove();
+  }, [remove, removeAccessToken]);
+
+  useEffect(() => {
+    if (!isError) return;
+    toast.error("Session expired, please login again");
+    signout();
+  }, [isError, signout]);
 
   return (
-    <AuthContext.Provider value={{ signedIn: !!accessToken, signin, signout }}>
-      {children}
+    <AuthContext.Provider
+      value={{
+        signedIn: isSuccess && !!accessToken,
+        signin,
+        signout,
+      }}
+    >
+      <LaunchScreen loading={isFetching} />
+      {!isFetching  && children}
     </AuthContext.Provider>
   );
 }
