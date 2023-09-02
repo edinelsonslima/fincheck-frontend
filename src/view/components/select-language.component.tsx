1b97bc05535e4b54e9a0e19d14f3f11d289a26f7
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-02 19:25:55 -0300

    get and set language with context of language

--- src/view/components/select-language.component.tsx
+++ src/view/components/select-language.component.tsx
@@ -1,35 +1,27 @@
-import { ComponentProps, useEffect } from "react";
-import { useLocalStorage } from "../../app/hooks/use-local-storage.hook";
-import { DEFAULT_LANGUAGE, LANGUAGES, ILanguages } from "../../app/i18n";
-import { enLocalStorage } from "../../types/enums/local-storage.enum";
+import { LANGUAGES, ILanguages } from "../../app/i18n";
+
 import { cn } from "../../app/utils/cn.utils";
+import { useLanguage } from "../../app/hooks/use-language.hook";
+import { SelectProps } from "../../types/interfaces";
 
 interface IOnChangeLanguage extends React.ChangeEvent<HTMLSelectElement> {
   target: HTMLSelectElement & { value: ILanguages };
 }
 
-interface ISelectLanguageProps
-  extends Omit<ComponentProps<"select">, "children" | "value" | "onChange"> {}
+interface ISelectLanguageProps extends SelectProps {}
 
 export function SelectLanguage({ className, ...props }: ISelectLanguageProps) {
-  const [language, setLanguage] = useLocalStorage(
-    enLocalStorage.LANGUAGE,
-    DEFAULT_LANGUAGE
-  );
-
-  useEffect(() => {
-    document.documentElement.lang = language;
-  }, [language]);
+  const [language, setLanguage] = useLanguage();
 
   return (
     <select
       {...props}
+      value={language}
+      onChange={(e: IOnChangeLanguage) => setLanguage(e.target.value)}
       className={cn(
         "rounded-lg px-2 py-1 bg-teal-50 text-sm cursor-pointer outline-none border border-teal-100 text-teal-900",
         className
       )}
-      onChange={(e: IOnChangeLanguage) => setLanguage(e.target.value)}
-      value={language}
     >
       {LANGUAGES.map((lang) => (
         <option key={lang} label={lang} value={lang} />
