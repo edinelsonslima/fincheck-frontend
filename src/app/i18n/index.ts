4774aaeba1e6b2862ed422f930fb433b3c51ff57
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-02 22:58:18 -0300

    added currency code created function for format currency

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -1,26 +1,41 @@
 import { enLocalStorage } from "../../types/enums/local-storage.enum";
 
 export const langs = {
-  "pt-br": (await import("./langs/pt-br.json")).default,
-  "en-us": (await import("./langs/en-us.json")).default,
+  "pt-br": {
+    term: (await import("./langs/pt-br.json")).default,
+    currencyCode: "BRL",
+  },
+  "en-us": {
+    term: (await import("./langs/en-us.json")).default,
+    currencyCode: "USD",
+  },
 };
 
 export type ILanguages = keyof typeof langs;
-export const LANGUAGES = Object.keys(langs) as (keyof typeof langs)[];
-export const DEFAULT_LANGUAGE: keyof typeof langs = "en-us";
+type ITerms = keyof (typeof langs)[ILanguages]["term"];
 
-function getTranslatedTerm<T extends keyof typeof langs>(
-  lang: T,
-  term: keyof (typeof langs)[T]
-) {
-  return langs[lang][term] || term;
+function getTranslatedTerm<T extends ILanguages>(lang: T, term: ITerms) {
+  return langs[lang]["term"][term] || term;
 }
 
-function getLanguage(): keyof typeof langs {
+function getLanguage(): ILanguages {
   const language = localStorage.getItem(enLocalStorage.LANGUAGE);
   return language ? JSON.parse(language) : DEFAULT_LANGUAGE;
 }
 
-export function t(term: keyof (typeof langs)[typeof DEFAULT_LANGUAGE]) {
+export function t(term: ITerms) {
   return getTranslatedTerm(getLanguage(), term);
 }
+
+export function intlCurrency(value: number) {
+  const language = getLanguage();
+
+  return new Intl.NumberFormat(language, {
+    style: "currency",
+    currencyDisplay: "symbol",
+    currency: langs[language].currencyCode,
+  }).format(value);
+}
+
+export const LANGUAGES = Object.keys(langs) as ILanguages[];
+export const DEFAULT_LANGUAGE: ILanguages = "en-us";
