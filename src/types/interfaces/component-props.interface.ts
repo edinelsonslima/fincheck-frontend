f352388778c5ce615eff1f413964ba88494defb1
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-02 22:59:37 -0300

    created button props interface

--- src/types/interfaces/component-props.interface.ts
+++ src/types/interfaces/component-props.interface.ts
@@ -4,3 +4,4 @@ type OmitChildren<T> = Omit<T, "children">;
 
 export interface SVGProps extends OmitChildren<ComponentProps<"svg">> {}
 export interface SelectProps extends OmitChildren<ComponentProps<"select">> {}
+export interface ButtonProps extends OmitChildren<ComponentProps<"button">> {}
