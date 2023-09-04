635b8f52e82888017ab53f9b2241885893aa3aff
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-03 23:50:29 -0300

    created button month

---
+++ src/view/pages/dashboard/components/transactions/button-month.component.tsx
@@ -0,0 +1,23 @@
+import { useSwiper } from "swiper/react";
+
+interface ButtonMonthProps {
+  isActive: boolean;
+  month: { month: string; index: number };
+}
+
+export function ButtonMonth({
+  isActive,
+  month: { index, month },
+}: ButtonMonthProps) {
+  const swiper = useSwiper();
+
+  return (
+    <button
+      data-active={isActive}
+      onClick={() => swiper.slideTo(index)}
+      className="w-full h-12 rounded-full text-sm tracking-tighter font-medium text-gray-800 select-none data-[active='true']:bg-white"
+    >
+      {month}
+    </button>
+  );
+}
