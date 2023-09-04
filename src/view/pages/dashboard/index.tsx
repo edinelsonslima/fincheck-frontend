75252f3d74343cb8e8f053bfd8a543de2589085e
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-03 21:22:15 -0300

    adjustment mobile responsive

--- src/view/pages/dashboard/index.tsx
+++ src/view/pages/dashboard/index.tsx
@@ -12,11 +12,11 @@ export function Dashboard() {
       </header>
 
       <main className="flex flex-col md:flex-row gap-4 flex-1">
-        <section className="bg-teal-900 w-1/2 h-full flex flex-col rounded-2xl px-4 py-8 md:p-10">
+        <section className="bg-teal-900 h-full flex flex-col rounded-2xl px-4 py-8 md:p-10 md:w-1/2">
           <Accounts />
         </section>
 
-        <section className="bg-gray-100 w-1/2 h-full flex flex-col rounded-2xl p-10">
+        <section className="bg-gray-100 h-full flex flex-col rounded-2xl p-10 md:w-1/2">
           <Transactions />
         </section>
       </main>
