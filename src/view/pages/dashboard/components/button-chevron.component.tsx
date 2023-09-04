e2e0b4716a00978280975ec97f442a9fc6bf3264
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-03 23:43:57 -0300

    moved button chevron for component folder dash

---
+++ src/view/pages/dashboard/components/button-chevron.component.tsx
@@ -0,0 +1,20 @@
+import { useSwiper } from "swiper/react";
+import { IconChevronLeft } from "../../../../assets/icons/chevron-left.icon";
+import { IconChevronRight } from "../../../../assets/icons/chevron-left.right";
+import { ButtonProps } from "../../../../types/interfaces";
+
+interface ButtonChevronProps extends ButtonProps {
+  direction: "slidePrev" | "slideNext";
+}
+
+export function ButtonChevron({ direction, ...props }: ButtonChevronProps) {
+  const swiper = useSwiper();
+
+  return (
+    <button {...props} onClick={() => swiper[direction]()}>
+      {direction === "slidePrev" && <IconChevronLeft className="w-6 h-6" />}
+
+      {direction === "slideNext" && <IconChevronRight className="w-6 h-6" />}
+    </button>
+  );
+}
