2dc68bf9e7775cbead33e8229079111ecf3421eb
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-05 01:05:43 -0300

    fix not blur bug

--- src/view/pages/dashboard/components/value.component.tsx
+++ src/view/pages/dashboard/components/value.component.tsx
@@ -6,14 +6,20 @@ interface ValueProps {
   value: number;
   visible: boolean;
   className?: string;
-  blur?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "0" | "none";
+  blur?: "sm" | "md";
 }
 
 export function Value({ value, className, visible, blur = "md" }: ValueProps) {
   const { intlCurrency } = useMemo(() => intlService, []);
   const formattedValue = intlCurrency(value);
   return (
-    <span className={cn(className, !visible && `blur-${blur}`)}>
+    <span
+      className={cn(
+        className,
+        !visible && blur === "md" && "blur-md",
+        !visible && blur === "sm" && "blur-sm"
+      )}
+    >
       {visible && formattedValue}
       {!visible && formattedValue.replace(/[0-9]/g, "0")}
     </span>
