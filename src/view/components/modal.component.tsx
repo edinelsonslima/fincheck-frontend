2823bdb0d591a6dde7c3a20317a128f985f06ada
edinelsonslima <edinelsonlima86@gmail.com>
2023-09-11 21:08:07 -0300

    created component modal

---
+++ src/view/components/modal.component.tsx
@@ -0,0 +1,58 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { PropsWithChildren } from "react";
+import { cn } from "../../app/utils/cn.utils";
+import { IconCross } from "../../assets/icons/cross.icon";
+
+interface ModalProps {
+  open: boolean;
+  title: string;
+  onClose?(): void;
+  rightAction?: React.ReactNode;
+  className?: string;
+}
+
+export function Modal({
+  open,
+  title,
+  onClose,
+  children,
+  className,
+  rightAction,
+}: PropsWithChildren<ModalProps>) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          className={cn(
+            "z-10 fixed inset-0 bg-black/80 backdrop-blur-sm",
+            "data-[state=open]:animate-overlay-show"
+          )}
+        />
+        <Dialog.Content
+          className={cn(
+            "z-20 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+            "p-6 space-y-10 w-full max-w-[25rem] bg-white rounded-2xl shadow-[0_11px_20px_0_rgba(0,0,0,0.10)] outline-none",
+            "data-[state=open]:animate-content-show"
+          )}
+        >
+          <header className="h-12 flex items-center justify-between text-gray-800">
+            <button
+              onClick={onClose}
+              className="w-12 h-full flex items-center justify-center outline-none"
+            >
+              <IconCross className="w-6 h-6" />
+            </button>
+
+            <span className="text-lg tracking-tightest font-bold">{title}</span>
+
+            <div className="w-12 h-full flex items-center justify-center">
+              {rightAction}
+            </div>
+          </header>
+
+          <div className={className}>{children}</div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
