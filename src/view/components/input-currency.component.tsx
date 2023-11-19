30be2ab58f09f0f42e2590cb720f743faf4b472a
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-19 16:59:50 -0300

    first version of the input currency

--- src/view/components/input-currency.component.tsx
+++ src/view/components/input-currency.component.tsx
@@ -1,11 +1,4 @@
-// import { NumericFormat } from "react-number-format";
-// <NumericFormat
-//   thousandSeparator="."
-//   decimalSeparator=","
-//
-// />
-
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useRef } from "react";
 import { intlService } from "../../app/services/intl.service";
 
 interface InputCurrencyEvent extends FormEvent<HTMLInputElement> {
@@ -15,22 +8,32 @@ interface InputCurrencyEvent extends FormEvent<HTMLInputElement> {
 }
 
 export function InputCurrency() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: InputCurrencyEvent) => {
+    if (!inputRef.current) return;
+    const { selectionStart, selectionEnd } = inputRef.current;
+
+    const value = e.target.value
+      .replace(/[.,](?![^.,]*$)/g, "")
+      .replace(",", ".")
+      .replace(/\.0+$/g, "")
+      .replace(/[^\d.]/g, "");
+
+    const { priceValue } = intlService.intlCurrency(Number(value));
+    e.target.value = priceValue;
+
+    inputRef.current.setSelectionRange((selectionStart || 0) + 1, selectionEnd);
+  };
+
+  useEffect(() => inputRef.current?.focus(), [inputRef]);
+
   return (
     <input
-      className="text-gray-800 text-[2rem] font-bold tracking-tightest outline-none"
-      defaultValue={intlService.intlCurrency(0)}
-      // onInput={(event: InputCurrencyEvent) => {
-      //   const value = Number(event.target.value.replace(/[^0-9]/g, ""));
-      //   event.target.value = intlService.intlCurrency(value);
-      // }}
-      // onChange={(event: InputCurrencyEvent) => {
-      //   const value = Number(event.target.value.replace(/[^0-9]/g, ""));
-      //   event.target.value = intlService.intlCurrency(value);
-      // }}
-      onBlur={(event: InputCurrencyEvent) => {
-        const value = Number(event.target.value.replace(/[^0-9]/g, ""));
-        event.target.value = intlService.intlCurrency(value);
-      }}  
+      ref={inputRef}
+      className="w-full text-gray-900 text-[2rem] font-bold tracking-tightest outline-none"
+      placeholder={intlService.intlCurrency(Number(0)).priceValue}
+      onChange={handleChange}
     />
   );
 }
