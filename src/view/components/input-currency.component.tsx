6647ded2dd138c94853589cd25a2ad52095c466f
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-21 19:23:30 -0300

    improved currency input

--- src/view/components/input-currency.component.tsx
+++ src/view/components/input-currency.component.tsx
@@ -1,5 +1,8 @@
 import { FormEvent, useEffect, useRef } from "react";
 import { intlService } from "../../app/services/intl.service";
+import { IconCrossCircled } from "../../assets/icons/cross-circled.icon";
+import { cn } from "../../app/utils/cn.utils";
+import { currencyStringToNumber } from "../../app/utils/currency-string-to-number";
 
 interface InputCurrencyEvent extends FormEvent<HTMLInputElement> {
   target: HTMLInputElement & {
@@ -7,23 +10,35 @@ interface InputCurrencyEvent extends FormEvent<HTMLInputElement> {
   };
 }
 
+interface InputCurrencyProps {
+  error?: string;
+  value?: string;
+  onChange?(value: string): void;
+}
+
 const { intlCurrency } = intlService;
 
-export function InputCurrency() {
+export function InputCurrency({ error, onChange, value }: InputCurrencyProps) {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const getFormattedPrice = (value = "") => {
+    const { priceValue } = intlCurrency(currencyStringToNumber(value));
+    return priceValue;
+  };
+
   const handleChange = (e: InputCurrencyEvent) => {
+    const hasValue = Number(e.target.value.replace(/\D/g, ""));
+    if (!hasValue) {
+      onChange?.("");
+      return (e.target.value = "");
+    }
+
     if (!inputRef.current) return;
     const { selectionStart, selectionEnd } = inputRef.current;
 
-    const value = e.target.value
-      .replace(/[.,](?![^.,]*$)/g, "")
-      .replace(",", ".")
-      .replace(/\.0+$/g, "")
-      .replace(/[^\d.]/g, "");
-
-    const { priceValue } = intlCurrency(Number(value));
-    e.target.value = priceValue;
+    const price = getFormattedPrice(e.target.value);
+    e.target.value = price;
+    onChange?.(price);
 
     inputRef.current.setSelectionRange((selectionStart || 0) + 1, selectionEnd);
   };
@@ -31,11 +46,24 @@ export function InputCurrency() {
   useEffect(() => inputRef.current?.focus(), [inputRef]);
 
   return (
-    <input
-      ref={inputRef}
-      className="w-full text-gray-900 text-[2rem] font-bold tracking-tightest outline-none"
-      placeholder={intlCurrency(0).priceValue}
-      onChange={handleChange}
-    />
+    <div>
+      <input
+        ref={inputRef}
+        onChange={handleChange}
+        placeholder={getFormattedPrice()}
+        defaultValue={value ? getFormattedPrice(value) : undefined}
+        className={cn(
+          "w-full text-gray-900 text-[2rem] font-bold tracking-tightest outline-none",
+          error && "text-red-900 placeholder:text-red-900/50"
+        )}
+      />
+
+      {error && (
+        <div className="flex items-center gap-2 mt-2 text-red-900">
+          <IconCrossCircled />
+          <span className=" text-xs">{error}</span>
+        </div>
+      )}
+    </div>
   );
 }
