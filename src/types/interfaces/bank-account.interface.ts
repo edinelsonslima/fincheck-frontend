2148265235c52e34538b650a214b1122ddb0b153
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-22 20:15:17 -0300

    bank account interfaces

---
+++ src/types/interfaces/bank-account.interface.ts
@@ -0,0 +1,51 @@
+import { AxiosError } from "axios";
+import { IApiError, OmitMKeyMFn, OmitQKeyQFn } from ".";
+import {
+  QueryFunctionContext,
+  UseMutationOptions,
+  UseQueryOptions,
+} from "@tanstack/react-query";
+import { enBankAccountType } from "../enums/bank-account-type.enum";
+
+export module IBankAccountCreate {
+  export interface Params {
+    name: string;
+    initialBalance: number;
+    color: string;
+    type: keyof typeof enBankAccountType;
+  }
+
+  export interface Response {
+    color: string;
+    id: string;
+    initialBalance: number;
+    name: string;
+    type: keyof typeof enBankAccountType;
+    userId: string;
+  }
+
+  export interface Error extends AxiosError<IApiError, Params> {}
+
+  export interface MutationOptions
+    extends OmitMKeyMFn<UseMutationOptions<Response, Error, Params>> {}
+}
+
+export module IBankAccountGetAll {
+  export interface Response
+    extends Array<{
+      color: string;
+      id: string;
+      initialBalance: number;
+      name: string;
+      type: keyof typeof enBankAccountType;
+      userId: string;
+      currentBalance: number;
+    }> {}
+
+  export interface Error extends AxiosError<IApiError> {}
+
+  export interface QueryOptions
+    extends OmitQKeyQFn<UseQueryOptions<Response, Error, Response, string[]>> {}
+
+  export interface QueryFnProps extends QueryFunctionContext {}
+}
