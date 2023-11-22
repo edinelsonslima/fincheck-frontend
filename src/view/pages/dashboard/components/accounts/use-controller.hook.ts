2207746dee38335f77979511e606fae5db36fca3
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-22 20:29:00 -0300

    get all accounts and calculate current balance

--- src/view/pages/dashboard/components/accounts/use-controller.hook.ts
+++ src/view/pages/dashboard/components/accounts/use-controller.hook.ts
@@ -2,10 +2,12 @@ import { useMemo, useState } from "react";
 import { useWindowSize } from "../../../../../app/hooks/use-window-size";
 import { intlService } from "../../../../../app/services/intl.service";
 import { useDashboard } from "../../hook/use-dashboard.hook";
+import { useBankAccountGetAll } from "../../../../../app/hooks/use-bank-account.hook";
 
 const { intlTerm } = intlService;
 
 export function useController() {
+  const { data, isFetching } = useBankAccountGetAll();
   const [windowWidth] = useWindowSize();
   const { areValuesVisible, toggleValuesVisibility, openNewAccountModal } =
     useDashboard();
@@ -20,6 +22,12 @@ export function useController() {
     return isLess(500) ? 1.1 : windowWidth.md ? 2.1 : isLess(900) ? 1.1 : 2.1;
   }, [windowWidth.md, windowWidth.width]);
 
+  const currentBalance = useMemo(() => {
+    if (!data) return 0;
+
+    return data.reduce((total, account) => total + account.currentBalance, 0);
+  }, [data]);
+
   return {
     sliderState,
     setSliderState,
@@ -28,7 +36,8 @@ export function useController() {
     toggleValuesVisibility,
     openNewAccountModal,
     intlTerm,
-    isLoading: false,
-    accounts: [],
+    isLoading: isFetching,
+    accounts: data ?? [],
+    currentBalance,
   };
 }
