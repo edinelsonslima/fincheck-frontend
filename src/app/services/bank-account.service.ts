b39a8d49ccc351ae5f58789de00cc6b88f25ef49
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-23 00:27:28 -0300

    created update method

--- src/app/services/bank-account.service.ts
+++ src/app/services/bank-account.service.ts
@@ -1,17 +1,15 @@
-import {
-  IBankAccountCreate,
-  IBankAccountGetAll,
-} from "../../types/interfaces/bank-account.interface";
+import { IBankAccount } from "../../types/interfaces/bank-account.interface";
 import httpClient, { HttpClient } from "./http-client.service";
 
 class BankAccountService {
   constructor(private readonly _httpClient: HttpClient) {
     this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
     this.getAll = this.getAll.bind(this);
   }
 
-  public async create(params: IBankAccountCreate.Params) {
-    const { data } = await this._httpClient.post<IBankAccountCreate.Response>(
+  public async create(params: IBankAccount.Create.Params) {
+    const { data } = await this._httpClient.post<IBankAccount.Create.Response>(
       "/bank-accounts",
       params
     );
@@ -19,8 +17,17 @@ class BankAccountService {
     return data;
   }
 
-  public async getAll({ signal }: IBankAccountGetAll.QueryFnProps) {
-    const { data } = await this._httpClient.get<IBankAccountGetAll.Response>(
+  public async update(params: IBankAccount.Update.Params) {
+    const { data } = await this._httpClient.put<IBankAccount.Update.Response>(
+      "/bank-accounts",
+      params
+    );
+
+    return data;
+  }
+
+  public async getAll({ signal }: IBankAccount.GetAll.QueryFnProps) {
+    const { data } = await this._httpClient.get<IBankAccount.GetAll.Response>(
       "/bank-accounts",
       { signal }
     );
