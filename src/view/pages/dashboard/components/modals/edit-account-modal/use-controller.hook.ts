83385deca8a7c27a9685285de8cef81d7358ebf3
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-23 19:28:22 -0300

    created edit bank account modal

---
+++ src/view/pages/dashboard/components/modals/edit-account-modal/use-controller.hook.ts
@@ -0,0 +1,80 @@
+import { z } from "zod";
+import { intlService } from "../../../../../../app/services/intl.service";
+import { useDashboard } from "../../../hook/use-dashboard.hook";
+import { enBankAccountType } from "../../../../../../types/enums/bank-account-type.enum";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useQueryClient } from "@tanstack/react-query";
+import { enKeys } from "../../../../../../types/enums/requests-keys.enum";
+import { IBankAccount } from "../../../../../../types/interfaces/bank-account.interface";
+import { useBankAccountUpdate } from "../../../../../../app/hooks/use-bank-account.hook";
+
+const { intlCurrency, intlTerm } = intlService;
+
+const schema = z.object({
+  initialBalance: z.number({
+    required_error: intlTerm("Initial balance is required"),
+  }),
+  name: z.string().nonempty(intlTerm("Account name is required")),
+  color: z.string().nonempty(intlTerm("Color is required")),
+  type: z.enum(
+    Object.values(enBankAccountType) as [keyof typeof enBankAccountType]
+  ),
+});
+
+type IFormData = z.infer<typeof schema>;
+
+export function useController() {
+  const { mutateAsync, isLoading } = useBankAccountUpdate();
+  const queryClient = useQueryClient();
+
+  const { isEditAccountModalOpen, closeEditAccountModal, accountBeingEdited } =
+    useDashboard();
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit: hookFormHandleSubmit,
+    control,
+  } = useForm<IFormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      initialBalance: accountBeingEdited?.initialBalance,
+      type: accountBeingEdited?.type,
+      color: accountBeingEdited?.color,
+      name: accountBeingEdited?.name,
+    },
+  });
+
+  const { currencySymbol } = intlCurrency(0);
+
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      await mutateAsync({ ...data, id: accountBeingEdited!.id });
+
+      queryClient.invalidateQueries({ queryKey: enKeys.bankAccount.getAll });
+      toast.success(intlTerm("Account edited successfully!"));
+      closeEditAccountModal();
+    } catch (error) {
+      const err = error as IBankAccount.Update.Error;
+      toast.error(
+        intlTerm(
+          err.response?.data.message || "Error while editing the account!"
+        )
+      );
+    }
+  });
+
+  return {
+    isEditAccountModalOpen,
+    closeEditAccountModal,
+    intlTerm,
+    currencySymbol,
+    register,
+    errors,
+    handleSubmit,
+    control,
+    isLoading,
+  };
+}
