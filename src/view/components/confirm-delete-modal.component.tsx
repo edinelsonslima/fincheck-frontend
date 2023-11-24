89cafd694504cd4f4324929a870cba69b7b57437
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-23 21:33:17 -0300

    created modal for confirm delete bank account

---
+++ src/view/components/confirm-delete-modal.component.tsx
@@ -0,0 +1,58 @@
+import { intlService } from "../../app/services/intl.service";
+import { IconTrash } from "../../assets/icons/trash.icon";
+import { Button } from "./button.component";
+import { Modal } from "./modal.component";
+
+interface IConfirmDeleteModalProps {
+  onConfirm(): void;
+  onClose(): void;
+  title: string;
+  description?: string;
+  loading?: boolean;
+}
+
+const { intlTerm } = intlService;
+
+export function ConfirmDeleteModal({
+  onClose,
+  onConfirm,
+  title,
+  description,
+  loading,
+}: IConfirmDeleteModalProps) {
+  return (
+    <Modal
+      open
+      title={intlTerm("Delete")}
+      onClose={onClose}
+      className="flex flex-col items-center text-center gap-6 text-gray-800"
+    >
+      <div className="w-[3.25rem] h-[3.25rem] rounded-full bg-red-50 flex items-center justify-center">
+        <IconTrash className="w-6 h-6 stroke-red-900" />
+      </div>
+      <p className="max-w-[11.25rem] font-bold tracking-tighter">{title}</p>
+
+      {description && <p className="tracking-tighter">{description}</p>}
+
+      <div className="mt-10 space-y-4">
+        <Button
+          variant="danger"
+          className="w-full"
+          onClick={onConfirm}
+          loading={loading}
+        >
+          {intlTerm("Yes, I want to delete.")}
+        </Button>
+
+        <Button
+          variant="ghost"
+          className="w-full"
+          onClick={onClose}
+          disabled={loading}
+        >
+          {intlTerm("Cancel")}
+        </Button>
+      </div>
+    </Modal>
+  );
+}
