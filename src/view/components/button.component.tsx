b04ea2c0aec0c966b2f80290e12e403ebee148a2
edinelsonslima <edinelsonlima86@gmail.com>
2023-11-23 21:32:50 -0300

    created variants danger and ghost

--- src/view/components/button.component.tsx
+++ src/view/components/button.component.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "../../assets/animations/spinner.animation";
 
 interface IButtonProps extends ComponentProps<"button"> {
   loading?: boolean;
+  variant?: "danger" | "ghost";
 }
 
 export function Button({
@@ -11,21 +12,23 @@ export function Button({
   loading,
   disabled,
   children,
+  variant,
   ...props
 }: IButtonProps) {
   return (
-    <>
-      <button
-        {...props}
-        disabled={disabled ?? loading}
-        className={cn(
-          "px-6 h-12 rounded-2xl font-medium transition-all bg-teal-800 text-white hover:bg-teal-800 active:bg-teal-900 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed",
-          className
-        )}
-      >
-        {!loading && children}
-        {loading && <Spinner className="mx-auto w-6 h-6" />}
-      </button>
-    </>
+    <button
+      {...props}
+      disabled={disabled ?? loading}
+      className={cn(
+        "px-6 h-12 rounded-2xl font-medium transition-all bg-teal-900 text-white hover:bg-teal-800 active:bg-teal-900 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed",
+        variant === "danger" && "bg-red-900 hover:bg-red-800",
+        variant === "ghost" &&
+          "bg-transparent hover:bg-gray-800/5 text-gray-800 border border-gray-800",
+        className
+      )}
+    >
+      {!loading && children}
+      {loading && <Spinner className="mx-auto w-6 h-6" />}
+    </button>
   );
 }
