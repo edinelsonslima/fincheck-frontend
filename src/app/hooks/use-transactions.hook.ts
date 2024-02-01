8f0c34e83f6219210d7628fd4736b5f5912b3853
edinelsonslima <edinelsonlima86@gmail.com>
2024-01-31 21:16:08 -0300

    [working] created transaction hook

--- src/app/hooks/use-transactions.hook.ts
+++ src/app/hooks/use-transactions.hook.ts
@@ -1,7 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { enKeys } from "../../types/enums/requests-keys.enum";
 import { ITransactions } from "../../types/interfaces/transactions.interface";
 import { transactionsService } from "../services/transactions.service";
+
+export function useTransactionsGetAll(
+  options?: ITransactions.GetAll.QueryOptions
+) {
+  return useQuery(
+    enKeys.transactions.getAll,
+    transactionsService.getAll({ month: 1, year: 2024 }),
+    options
+  );
+}
 
 export function useTransactionsCreate(
   mutationOptions?: ITransactions.Create.MutationOptions
