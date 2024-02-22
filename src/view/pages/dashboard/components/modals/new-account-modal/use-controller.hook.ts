8ea6cb677d9202e42f7877c71e268d06113c2d9c
edinelsonslima <edinelsonlima86@gmail.com>
2024-02-22 01:48:14 -0300

    calc transactions value in local cache

--- src/view/pages/dashboard/components/modals/new-account-modal/use-controller.hook.ts
+++ src/view/pages/dashboard/components/modals/new-account-modal/use-controller.hook.ts
@@ -9,6 +9,9 @@ import toast from "react-hot-toast";
 import { useQueryClient } from "@tanstack/react-query";
 import { enKeys } from "../../../../../../types/enums/requests-keys.enum";
 import { IBankAccount } from "../../../../../../types/interfaces/bank-account.interface";
+import { ITransactions } from "../../../../../../types/interfaces/transactions.interface";
+import { useParameters } from "../../../../../../app/hooks/use-parameters.hook";
+import { enTransactionType } from "../../../../../../types/enums/transaction-type.enum";
 
 const { intlCurrency, intlTerm } = intlService;
 
@@ -31,6 +34,8 @@ export function useController() {
   const { isNewAccountModalOpen, closeNewAccountModal } = useDashboard();
   const { currencySymbol } = intlCurrency(0);
 
+  const [parameters] = useParameters();
+
   const bankAccountCreate = useBankAccountCreate();
   const queryClient = useQueryClient();
 
@@ -50,12 +55,49 @@ export function useController() {
     },
   });
 
+  const updateCacheBankAccounts = (
+    bankAccount: IBankAccount.Create.Response
+  ) => {
+    const transactions =
+      queryClient.getQueryData<ITransactions.GetAll.Response>(
+        enKeys.transactions.getAll({
+          month: parameters.month,
+          year: parameters.year,
+          type: parameters.type,
+          bankAccountId: parameters.bankAccountId,
+        })
+      );
+
+    const currentBalance = transactions
+      ?.filter(({ bankAccountId }) => bankAccountId === bankAccount.id)
+      .reduce((total, { value, type }) => {
+        if (type === enTransactionType.INCOME) {
+          total += value;
+        }
+
+        if (type === enTransactionType.EXPENSE) {
+          total -= value;
+        }
+
+        return total;
+      }, bankAccount.initialBalance);
+
+    queryClient.setQueryData<IBankAccount.GetAll.Response>(
+      enKeys.bankAccount.getAll,
+      (currencyBankAccount) =>
+        currencyBankAccount?.concat({
+          ...bankAccount,
+          currentBalance: currentBalance ?? 0,
+        })
+    );
+  };
+
   const handleSubmit = hookFormHandleSubmit(async (data) => {
     try {
-      await bankAccountCreate.mutateAsync(data);
+      const newBankAccount = await bankAccountCreate.mutateAsync(data);
 
       reset();
-      queryClient.invalidateQueries({ queryKey: enKeys.bankAccount.getAll });
+      updateCacheBankAccounts(newBankAccount);
       toast.success(intlTerm("Account registered successfully!"));
       closeNewAccountModal();
     } catch (error) {
