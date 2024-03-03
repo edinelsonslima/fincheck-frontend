f7f79f140689840ecf3bf6cf6b3d6dd854d4c932
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-03 14:31:52 -0300

    refactor: move update cache for hook create bank account - change try/catch for then/catch

--- src/app/hooks/use-bank-account.hook.ts
+++ src/app/hooks/use-bank-account.hook.ts
@@ -2,25 +2,60 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { IBankAccount } from "../../types/interfaces/bank-account.interface";
 import { bankAccountService } from "../services/bank-account.service";
 import { enKeys } from "../../types/enums/requests-keys.enum";
+import { useParameters } from "./use-parameters.hook";
+import { useCache } from "./use-cache.hook";
+import { ITransactions } from "../../types/interfaces/transactions.interface";
+import { enTransactionType } from "../../types/enums/transaction-type.enum";
 
-export function useBankAccountGet(
-  options?: IBankAccount.Get.QueryOptions
-) {
-  return useQuery(
-    enKeys.bankAccount.get,
-    bankAccountService.get,
-    options
-  );
+export function useBankAccountGet(options?: IBankAccount.Get.QueryOptions) {
+  return useQuery(enKeys.bankAccount.get, bankAccountService.get, options);
 }
 
 export function useBankAccountCreate(
   mutationOptions?: IBankAccount.Create.MutationOptions
 ) {
-  return useMutation(
-    enKeys.bankAccount.create,
-    bankAccountService.create,
-    mutationOptions
+  const [parameters] = useParameters();
+  const [, setCacheBankAccounts] = useCache<IBankAccount.Get.Response>(
+    enKeys.bankAccount.get
   );
+  const [getCacheTransactions] = useCache<ITransactions.Get.Response>(
+    enKeys.transactions.get({
+      month: parameters.month,
+      year: parameters.year,
+      type: parameters.type,
+      bankAccountId: parameters.bankAccountId,
+    })
+  );
+
+  const updateCacheBankAccounts = (
+    bankAccount: IBankAccount.Create.Response
+  ) => {
+    const currentBalance = getCacheTransactions()
+      ?.filter(({ bankAccountId }) => bankAccountId === bankAccount.id)
+      .reduce((total, { value, type }) => {
+        if (type === enTransactionType.INCOME) {
+          total += value;
+        }
+
+        if (type === enTransactionType.EXPENSE) {
+          total -= value;
+        }
+
+        return total;
+      }, bankAccount.initialBalance);
+
+    setCacheBankAccounts((bankAccounts) =>
+      bankAccounts?.concat({
+        ...bankAccount,
+        currentBalance: currentBalance ?? bankAccount.initialBalance,
+      })
+    );
+  };
+
+  return useMutation(enKeys.bankAccount.create, bankAccountService.create, {
+    ...mutationOptions,
+    onSuccess: updateCacheBankAccounts,
+  });
 }
 
 export function useBankAccountUpdate(
--- src/view/pages/dashboard/accounts/new-account-modal/use-controller.hook.ts
+++ src/view/pages/dashboard/accounts/new-account-modal/use-controller.hook.ts
@@ -5,12 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useBankAccountCreate } from "../../../../../app/hooks/use-bank-account.hook";
 import toast from "react-hot-toast";
-import { enKeys } from "../../../../../types/enums/requests-keys.enum";
 import { IBankAccount } from "../../../../../types/interfaces/bank-account.interface";
-import { ITransactions } from "../../../../../types/interfaces/transactions.interface";
-import { useParameters } from "../../../../../app/hooks/use-parameters.hook";
-import { enTransactionType } from "../../../../../types/enums/transaction-type.enum";
-import { useCache } from "../../../../../app/hooks/use-cache.hook";
 import { useDashboard } from "../../use-controller.hook";
 
 const { t } = intlService;
@@ -33,28 +28,8 @@ type IFormData = z.infer<typeof schema>;
 export function useController() {
   const { isNewAccountModalOpen, closeNewAccountModal } = useDashboard();
 
-  const [parameters] = useParameters();
-  const [, setCacheBankAccounts] = useCache<IBankAccount.Get.Response>(
-    enKeys.bankAccount.get
-  );
-  const [getCacheTransactions] = useCache<ITransactions.Get.Response>(
-    enKeys.transactions.get({
-      month: parameters.month,
-      year: parameters.year,
-      type: parameters.type,
-      bankAccountId: parameters.bankAccountId,
-    })
-  );
-
   const bankAccountCreate = useBankAccountCreate();
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit: hookFormHandleSubmit,
-    control,
-    reset,
-  } = useForm<IFormData>({
+  const form = useForm<IFormData>({
     resolver: zodResolver(schema),
     defaultValues: {
       initialBalance: 0,
@@ -64,57 +39,29 @@ export function useController() {
     },
   });
 
-  const updateCacheBankAccounts = (
-    bankAccount: IBankAccount.Create.Response
-  ) => {
-    const currentBalance = getCacheTransactions()
-      ?.filter(({ bankAccountId }) => bankAccountId === bankAccount.id)
-      .reduce((total, { value, type }) => {
-        if (type === enTransactionType.INCOME) {
-          total += value;
-        }
-
-        if (type === enTransactionType.EXPENSE) {
-          total -= value;
-        }
-
-        return total;
-      }, bankAccount.initialBalance);
-
-    setCacheBankAccounts((bankAccounts) =>
-      bankAccounts?.concat({
-        ...bankAccount,
-        currentBalance: currentBalance ?? bankAccount.initialBalance,
+  const handleSubmit = form.handleSubmit((data) => {
+    bankAccountCreate
+      .mutateAsync(data)
+      .then(() => {
+        toast.success(t("Account registered successfully!"));
+        form.reset();
+        closeNewAccountModal();
       })
-    );
-  };
-
-  const handleSubmit = hookFormHandleSubmit(async (data) => {
-    try {
-      const newBankAccount = await bankAccountCreate.mutateAsync(data);
-
-      reset();
-      updateCacheBankAccounts(newBankAccount);
-      toast.success(t("Account registered successfully!"));
-      closeNewAccountModal();
-    } catch (error) {
-      const err = error as IBankAccount.Create.Error;
-      toast.error(
-        t(
-          err.response?.data.message || "Error while registering the account!"
-        )
-      );
-    }
+      .catch((error) => {
+        const err = error as IBankAccount.Create.Error;
+        const defaultMessage = "Error while registering the account!";
+        toast.error(t(err.response?.data.message ?? defaultMessage));
+      });
   });
 
   return {
-    isNewAccountModalOpen,
-    closeNewAccountModal,
     t,
-    register,
-    errors,
     handleSubmit,
-    control,
+    closeNewAccountModal,
+    control: form.control,
+    isNewAccountModalOpen,
+    register: form.register,
+    errors: form.formState.errors,
     isLoading: bankAccountCreate.isLoading,
   };
 }
