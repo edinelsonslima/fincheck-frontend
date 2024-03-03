d329de0945db509e1c955b6af027bacc018d3cca
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-03 12:09:25 -0300

    refactor: organize functions

--- src/view/pages/dashboard/accounts/edit-account-modal/use-controller.hook.ts
+++ src/view/pages/dashboard/accounts/edit-account-modal/use-controller.hook.ts
@@ -1,21 +1,21 @@
-import { z } from "zod";
+import { useState } from "react";
+import { useParameters } from "../../../../../app/hooks/use-parameters.hook";
 import { intlService } from "../../../../../app/services/intl.service";
-import { enBankAccountType } from "../../../../../types/enums/bank-account-type.enum";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-import { enKeys } from "../../../../../types/enums/requests-keys.enum";
+import { useDashboard } from "../../use-controller.hook";
+import { useCache } from "../../../../../app/hooks/use-cache.hook";
 import { IBankAccount } from "../../../../../types/interfaces/bank-account.interface";
+import { ITransactions } from "../../../../../types/interfaces/transactions.interface";
+import { enKeys } from "../../../../../types/enums/requests-keys.enum";
 import {
   useBankAccountDelete,
   useBankAccountUpdate,
 } from "../../../../../app/hooks/use-bank-account.hook";
-import { useState } from "react";
-import { ITransactions } from "../../../../../types/interfaces/transactions.interface";
-import { useParameters } from "../../../../../app/hooks/use-parameters.hook";
-import { useCache } from "../../../../../app/hooks/use-cache.hook";
+import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { enBankAccountType } from "../../../../../types/enums/bank-account-type.enum";
 import { enTransactionType } from "../../../../../types/enums/transaction-type.enum";
-import { useDashboard } from "../../use-controller.hook";
 
 const { t } = intlService;
 
@@ -36,13 +36,11 @@ export function useController() {
   const { isEditAccountModalOpen, closeEditAccountModal, accountBeingEdited } =
     useDashboard();
 
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [parameters] = useParameters();
-
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [, setCacheBankAccounts] = useCache<IBankAccount.GetAll.Response>(
     enKeys.bankAccount.getAll
   );
-
   const [getCacheTransactions, setCacheTransactions] =
     useCache<ITransactions.GetAll.Response>(
       enKeys.transactions.getAll({
@@ -106,6 +104,18 @@ export function useController() {
     }
   };
 
+  const updateCacheBankAccountsDeleted = () => {
+    setCacheTransactions((transactions) =>
+      transactions?.filter(
+        ({ bankAccountId }) => bankAccountId !== accountBeingEdited?.id
+      )
+    );
+
+    setCacheBankAccounts((bankAccounts) =>
+      bankAccounts?.filter(({ id }) => id !== accountBeingEdited?.id)
+    );
+  };
+
   const updateCacheBankAccountsEdited = (
     bankAccount: IBankAccount.Update.Response
   ) => {
@@ -146,18 +156,6 @@ export function useController() {
     });
   };
 
-  const updateCacheBankAccountsDeleted = () => {
-    setCacheTransactions((transactions) =>
-      transactions?.filter(
-        ({ bankAccountId }) => bankAccountId !== accountBeingEdited?.id
-      )
-    );
-
-    setCacheBankAccounts((bankAccounts) =>
-      bankAccounts?.filter(({ id }) => id !== accountBeingEdited?.id)
-    );
-  };
-
   const handleOpenDeleteModal = () => {
     setIsDeleteModalOpen(true);
   };
@@ -167,9 +165,9 @@ export function useController() {
   };
 
   return {
+    t,
     isEditAccountModalOpen,
     closeEditAccountModal,
-    t,
     register,
     errors,
     handleSubmit,
